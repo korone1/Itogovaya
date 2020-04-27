ea8a852f55b6d773ce3c5ed15513c1abc8c7ba86
korone <proctostas12@gmail.com>
2020-04-27 16:10:24 +0300

    add files

---
+++ Итогвая работа/Js/index.js
@@ -0,0 +1,22 @@
+/* window.addEventListener ("load", function () {
+    let menuBtn = document.querySelector(".smart__btn");
+    menuBtn.addEventListener("click", function() {
+    let navigation = document.querySelector ("navList");
+    navigation.classList.add("active");   
+    });
+}); */
+
+/* window.addEventListener( "load", function () {
+    let btn = document.querySelector (".smart__btn");
+    console.log (btn)
+    btn.addEventListener ("click", function () {
+       alert("Ну хоть что-то")
+    } )
+}); */
+window.addEventListener( "load", function () {
+    let btn = document.querySelector (".smart__btn");
+    btn.addEventListener ("click", function () {
+       let navigation = document.querySelector (".header__Navlist");
+       navigation.classList.toggle("active");  
+    });
+});
