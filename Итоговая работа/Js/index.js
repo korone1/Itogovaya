a4fcd5c563d863e32ee934c68d879b2ba958bc8f
korone <proctostas12@gmail.com>
2020-05-05 20:32:05 +0300

    add file about

--- Итоговая работа/Js/index.js
+++ Итоговая работа/Js/index.js
@@ -13,21 +13,42 @@
        alert("Ну хоть что-то")
     } )
 }); */
-window.addEventListener( "load", function () {
-    let btn = document.querySelector (".smart__btn");
-    btn.addEventListener ("click", function () {
-       let navigation = document.querySelector (".header__Navlist");
-       navigation.classList.toggle("active");  
-    });
+window.addEventListener("load", function() {
+  let btn = document.querySelector(".smart__btn");
+  btn.addEventListener("click", function() {
+    let navigation = document.querySelector(".header__Navlist");
+    navigation.classList.toggle("active");
+  });
 });
 
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel();
+});
+$(".owl-carousel").owlCarousel({
+  margin: 330,
+  autoWidth: true,
+  center: true,
+  loop: true,
+  nav: true,
+  dots: true
+});
+
+jQuery(document).ready(function() {
+  let btn = $("#buttonTop");
+  let animated = $(".avesomePics");
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 1000) {
+      btn.addClass("show");
+      animated.addClass('pulse');
+    } else {
+      btn.removeClass("show");
+      animated.reomoveClass('pulse');
+    }
   });
-  $('.owl-carousel').owlCarousel({
-    margin:330,
-    autoWidth:true,
-    center:true,
-    loop:true,
-    nav:true
-})
+
+  btn.on("click", function(e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "300");
+  });
+});
